a179362d9db8c2be63b4d91d1bfc3745d36ca628
stroyent <ankarubtsovsk@gmail.com>
2024-07-11 02:16:07 +0700

    transition and scale attempt

--- vanilla/basic.js
+++ vanilla/basic.js
@@ -10,6 +10,11 @@ window.onload = function() {
         div.style.margin = "-150px 0 0 -150px";
         div.style.backgroundColor = randomRGB();
         div.innerHTML = "This is a div.";
+        div.style.display = "flex";
+        div.style.flexFlow = "wrap";
+        div.style.placeContent = "center";
+        div.style.transition = "all 1s";
+        div.style.transform = "scale(1)";
         return div;
     }
     
@@ -23,18 +28,36 @@ window.onload = function() {
         return `rgb(${red} ${green} ${blue})`;
     }
 
-
     function changeElement(id) {
-        angle = (angle + 90) % 720;
         let element = document.getElementById(id);
+        
+        element.style.transition = "all 1s";
+        angle = angle + 90;
         element.style.backgroundColor = randomRGB();
-        element.style.transform = `rotate(${angle}deg)`;
-    }
+        element.style.transform = element.style.transform.split(' ')[0] + ` rotate(${angle}deg)`;
 
+        if (angle === 360) {
+            setTimeout(() => {
+            element.style.transition = "all 0s";
+            angle = 0;
+            element.style.transform = element.style.transform.split(' ')[0] + ` rotate(${angle}deg)`;
+            }, 0);
+        }
+    }
 
     const div = makeDiv();
     document.body.append(div);
+    
     let angle = 0;
-    setInterval(() => changeElement(div.id), 1000);
+    
+    setInterval(() => {
+        changeElement(div.id);
+        div.addEventListener("mouseenter", () => {
+            div.style.transform = "scale(1.2)" + ` rotate(${angle}deg)`;
+        });
+        div.addEventListener("mouseleave", () => {
+            div.style.transform = "scale(1)" + ` rotate(${angle}deg)`;
+        });
+    }, 1000);
 }
 
