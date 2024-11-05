9f07a25d054f9fcd22fdf9e9133f23d038295a7f
stroyent <ankarubtsovsk@gmail.com>
2024-11-05 19:24:26 +0300

    async generators

--- basics/generators.js
+++ basics/generators.js
@@ -12,3 +12,25 @@ let generator = pseudoRandom(1);
 alert(generator.next().value); // 16807
 alert(generator.next().value); // 282475249
 alert(generator.next().value); // 1622650073
+
+ // async generators
+
+ async function* sequence() {
+    let i = 1;
+    while(true) {
+        await new Promise(resolve => setTimeout(resolve, 1000))
+        i++;
+        yield i;
+    }
+ }
+
+
+(async () => {
+    let sequenceGenerator = sequence()
+    for await (let number of sequenceGenerator) {
+        alert(number);
+        if (number === 5) {
+            break;
+        }
+     }
+})()
